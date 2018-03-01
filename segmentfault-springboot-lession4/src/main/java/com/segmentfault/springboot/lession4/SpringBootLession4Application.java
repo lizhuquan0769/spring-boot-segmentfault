95e4878b40ee0dfbcc93f6629db13597084a7f3b
446995198@qq.com <446995198@qq.com>
2018-03-01 11:16:08 +0800

    jsp support

--- segmentfault-springboot-lession4/src/main/java/com/segmentfault/springboot/lession4/SpringBootLession4Application.java
+++ segmentfault-springboot-lession4/src/main/java/com/segmentfault/springboot/lession4/SpringBootLession4Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-//@ServletComponentScan(basePackages = {"com.segmentfault.springboot.lession4.servlet"})
+@ServletComponentScan(basePackages = {"com.segmentfault.springboot.lession4.servlet"})
 public class SpringBootLession4Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
