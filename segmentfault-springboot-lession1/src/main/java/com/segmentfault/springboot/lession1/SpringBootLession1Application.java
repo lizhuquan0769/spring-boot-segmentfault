3d7e2b2c4d60e6feaff44cd7baab6ebd0e2fbdae
DeaconLi <446995198@qq.com>
2018-03-04 00:05:25 +0800

    rename project

--- segmentfault-springboot-lession1/src/main/java/com/segmentfault/springboot/lession1/SpringBootLession1Application.java
+++ segmentfault-springboot-lession1/src/main/java/com/segmentfault/springboot/lession1/SpringBootLession1Application.java
@@ -1,4 +1,4 @@
-package com.segmentfault.lession1;
+package com.segmentfault.springboot.lession1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
